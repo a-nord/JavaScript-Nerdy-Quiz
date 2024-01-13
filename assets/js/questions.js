13fde5f46d1cba69af6ef02ade0a79c171ada5cd
Ashley Nord <anord@hntb.com>
2024-01-13 03:54:54 -0500

    added the questions to the js

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// Quiz Questions
+const questions = [
+    {
+        q: "Who is the author of the Bridgerton Series?",
+        a: ["Courtney Milan","Julia Quinn","Tessa Dare", "Sarah MacLean"],
+        c: "Julia Quinn"
+    },
+    {
+        q: "The Bridgerton Family is comprised of how many siblings",
+        a: ["2", "10", "8", "7"],
+        c: "8"
+    },
+    {
+        q: "Who is the 5th Bridgerton sibling?",
+        a: ["Eloise", "Benedict", "Daphne", "Hyacinth"],
+        c: "Eloise"
+    },
+    {
+        q: "Which lady ends up causing a scandal in Bridgerton (show) when it's found out she's pregnant outside of wedlock?",
+        a: ["Marina Thompson", "Penelope Featherington", "Daphne Bridgerton", "Hyacinth Bridgerton"],
+        c: "Marina Thompson"
+    },
+    {
+        q: "Who is revealed to be Lady Whistledown at the end of season 1?",
+        a: ["Eloise Bridgerton", "Lady Danbury", "Dan Humphrey(XOXO)", "Penelope Featherington"],
+        c: "Penelope Featherington"
+    }
+]
